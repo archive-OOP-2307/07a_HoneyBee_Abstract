27891bc17b4067dde77a2f0d513679f5a55e7775
Peter <schmidt.multimedia@gmail.com>
2023-08-03 10:32:55 +0200

    Bird() ++

--- com/cc/java/App.java
+++ com/cc/java/App.java
@@ -7,10 +7,12 @@ public class App {
         Queen queen = new Queen();
         Worker worker = new Worker();
         Drone drone = new Drone();
+        Bird bird = new Bird();
 
         pollObj(queen);
         pollObj(worker);
         pollObj(drone);
+        pollObj(bird);
 
         // output("---------------------");
 
---
+++ com/cc/java/Bird.java
@@ -0,0 +1,20 @@
+package com.cc.java;
+
+public class Bird extends HoneyBee{
+
+    @Override
+    public String doYourJob() {
+       return "But I'm a Biiiiiiird!";
+    }
+
+    @Override
+    public String fly() {
+        return "I'm a bird, I can also fly!";
+    }
+   
+    
+
+
+
+
+}
